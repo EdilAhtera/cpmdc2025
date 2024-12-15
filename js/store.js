4d4042f3dc48481395d7dabd633c12f80648facc
Ahtera Studio <eadlalamsa@gmail.com>
2024-12-15 08:46:48 +0700

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,50 +1,7 @@
 // store.js
-const productList = document.getElementById('product-list');
+import { loadProducts } from './firebase.js';
 
-// Memastikan Firestore sudah terhubung
-firebase.firestore().collection('produk').get()
-    .then(snapshot => {
-        if (snapshot.empty) {
-            console.log('Tidak ada produk dalam database');
-            return;
-        }
-
-        snapshot.forEach(doc => {
-            const product = doc.data();
-            const productDiv = document.createElement('div');
-            productDiv.classList.add('product-card');
-            productDiv.innerHTML = `
-                <h3>${product.nama}</h3>
-                <p>Harga: Rp ${product.harga}</p>
-                <p>Stok: ${product.sisa}</p>
-                <a href="product.html?id=${doc.id}">Lihat Detail</a>
-            `;
-            productList.appendChild(productDiv);
-        });
-    })
-    .catch(error => console.error("Error getting documents: ", error));
-
-
-firebase.firestore().collection('produk').get()
-    .then(snapshot => {
-        console.log("Data produk:", snapshot);
-        if (snapshot.empty) {
-            console.log('Tidak ada produk dalam database');
-            return;
-        }
-
-        snapshot.forEach(doc => {
-            const product = doc.data();
-            console.log("Produk:", product);
-            const productDiv = document.createElement('div');
-            productDiv.classList.add('product-card');
-            productDiv.innerHTML = `
-                <h3>${product.nama}</h3>
-                <p>Harga: Rp ${product.harga}</p>
-                <p>Stok: ${product.sisa}</p>
-                <a href="product.html?id=${doc.id}">Lihat Detail</a>
-            `;
-            productList.appendChild(productDiv);
-        });
-    })
-    .catch(error => console.error("Error getting documents: ", error));
+// Memanggil fungsi loadProducts untuk menampilkan produk
+document.addEventListener('DOMContentLoaded', function() {
+  loadProducts();
+});
