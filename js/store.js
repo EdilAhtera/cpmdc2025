25819e6606db3598e3d52da5b1e3689294fb8a85
Ahtera Studio <eadlalamsa@gmail.com>
2024-12-15 08:43:22 +0700

    Update store.js

--- js/store.js
+++ js/store.js
@@ -23,3 +23,28 @@ firebase.firestore().collection('produk').get()
         });
     })
     .catch(error => console.error("Error getting documents: ", error));
+
+
+firebase.firestore().collection('produk').get()
+    .then(snapshot => {
+        console.log("Data produk:", snapshot);
+        if (snapshot.empty) {
+            console.log('Tidak ada produk dalam database');
+            return;
+        }
+
+        snapshot.forEach(doc => {
+            const product = doc.data();
+            console.log("Produk:", product);
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('product-card');
+            productDiv.innerHTML = `
+                <h3>${product.nama}</h3>
+                <p>Harga: Rp ${product.harga}</p>
+                <p>Stok: ${product.sisa}</p>
+                <a href="product.html?id=${doc.id}">Lihat Detail</a>
+            `;
+            productList.appendChild(productDiv);
+        });
+    })
+    .catch(error => console.error("Error getting documents: ", error));
