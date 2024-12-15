ba2a96d01a7b7e8de63517f808a7ef934f68d369
Ahtera Studio <eadlalamsa@gmail.com>
2024-12-15 07:38:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDxMxtcxKYblIF9R1Izj6_OF0Zx-NU7zxY",
   authDomain: "tokoonline-561d8.firebaseapp.com",
-  databaseURL: "tokoonline-561d8",
+  databaseURL: "https://tokoonline-561d8-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "tokoonline-561d8",
   storageBucket: "tokoonline-561d8.firebasestorage.app",
   messagingSenderId: ""799667418330",",
