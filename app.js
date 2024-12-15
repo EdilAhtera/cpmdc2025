5306c0629931d5ac54cad01e14e3ced01f3e5be2
Ahtera Studio <eadlalamsa@gmail.com>
2024-12-15 08:07:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDxMxtcxKYblIF9R1Izj6_OF0Zx-NU7zxY",
   authDomain: "tokoonline-561d8.firebaseapp.com",
-  databaseURL: "https://tokoonline-561d8-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  databaseURL: "https://tokoonline-561d8-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "tokoonline-561d8",
   storageBucket: "tokoonline-561d8.firebasestorage.app",
-  messagingSenderId: ""799667418330",",
+  messagingSenderId: "799667418330",
   appId: "1:799667418330:web:44741de8e8db5596cde1e3"
 };
 
