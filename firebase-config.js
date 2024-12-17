d9d982644fc3927661399897bed9effa16af8b41
Ahtera Studio <eadlalamsa@gmail.com>
2024-12-17 17:34:48 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,6 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
-
-// Ganti dengan config Firebase project lu
+// Inisialisasi Firebase
+const firebaseConfig = {
 const firebaseConfig = {
   apiKey: "AIzaSyAuMS7491Px931fv_84sswIB4RaHnd2JHs",
   authDomain: "cpmdcstore25.firebaseapp.com",
@@ -10,8 +8,9 @@ const firebaseConfig = {
   projectId: "cpmdcstore25",
   storageBucket: "cpmdcstore25.firebasestorage.app",
   messagingSenderId: "154643475128",
-  appId: "1:154643475128:web:df2486b2d56e600d4de24d",
+  appId: "1:154643475128:web:df2486b2d56e600d4de24d"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
